bf5411c8bbf7ffcbf76710994f3046ced76bdc6e
Mohamed Fathy Abdrabo <42771081+mohamedFabdrabo@users.noreply.github.com>
2022-01-20 02:17:39 +0100

    Final

---
+++ project_dataset/segbench/Dataset/bsdsRoot.m
@@ -0,0 +1,9 @@
+function [root] = bsdsRoot()
+% function [root] = bsdsRoot()
+%
+% Return the root directory for the BSDS data.
+%
+% David R. Martin <dmartin@eecs.berkeley.edu>
+% March 2003
+
+root = 'E:\GUC files\semester 9\CV\New folder\BSDS300';
